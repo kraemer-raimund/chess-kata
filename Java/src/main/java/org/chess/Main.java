054472b92f0aa1759a55986e86607e4602257116
Raimund Krämer <kraemer_raimund@web.de>
2023-01-21 14:03:20 +0100

    Rename default package

--- Java/src/main/java/org/chess/Main.java
+++ Java/src/main/java/org/chess/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.chess;
 
 public class Main {
 
--- Java/src/test/java/org/chess/ExampleTest.java
+++ Java/src/test/java/org/chess/ExampleTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.chess;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
