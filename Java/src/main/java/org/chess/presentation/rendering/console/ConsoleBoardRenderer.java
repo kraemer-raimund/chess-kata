75ce871d4f05eb585b6430aec8fa7d8e53669e8f
Raimund Krämer <kraemer_raimund@web.de>
2023-01-21 17:40:22 +0100

    Render chess pieces on the board based on the provided gamestate

--- Java/src/main/java/org/chess/presentation/rendering/console/ConsoleBoardRenderer.java
+++ Java/src/main/java/org/chess/presentation/rendering/console/ConsoleBoardRenderer.java
@@ -1,6 +1,9 @@
 package org.chess.presentation.rendering.console;
 
+import org.chess.domain.ChessPiece;
 import org.chess.domain.GameState;
+import org.chess.domain.PlayerColor;
+import org.chess.domain.Position;
 
 public class ConsoleBoardRenderer {
 
@@ -26,15 +29,49 @@ public class ConsoleBoardRenderer {
                     a     b     c     d     e     f     g     h
             """;
 
-    private final char[] renderBuffer = new char[EMPTY_BOARD.length()];
+    private final char[] RENDER_BUFFER = new char[EMPTY_BOARD.length()];
 
     public String render(GameState gameState) {
         renderEmptyBoard();
+        renderChessPieces(gameState);
 
-        return new String(renderBuffer);
+        return new String(RENDER_BUFFER);
     }
 
     private void renderEmptyBoard() {
-        System.arraycopy(EMPTY_BOARD.toCharArray(), 0, renderBuffer, 0, renderBuffer.length);
+        System.arraycopy(EMPTY_BOARD.toCharArray(), 0, RENDER_BUFFER, 0, RENDER_BUFFER.length);
+    }
+
+    private void renderChessPieces(GameState gameState) {
+        gameState.chessPiecePositions().forEach(this::renderChessPiece);
+    }
+
+    private void renderChessPiece(Position position, ChessPiece chessPiece) {
+        final int horizontalOffsetForLabels = 5;
+        final int squareWidth = 6;
+        final int squareHeight = 2;
+        final int horizontalOffsetWithinSquare = 4;
+
+        // Rows on board are counted from bottom, but in the buffer from top.
+        final int rowFromTop = 8 - position.row();
+
+        final int initialEmptyLines = 1;
+        final int line = initialEmptyLines + rowFromTop * squareHeight;
+        final int offsetWithinLine = horizontalOffsetForLabels + (position.column() - 1) * squareWidth + horizontalOffsetWithinSquare;
+        final int charsPerLine = 55;
+        final int index = line * charsPerLine + offsetWithinLine;
+
+        RENDER_BUFFER[index] = toChar(chessPiece);
+    }
+
+    private char toChar(ChessPiece chessPiece) {
+        return switch (chessPiece.name()) {
+            case PAWN -> chessPiece.color() == PlayerColor.WHITE ? '♙' : '♟';
+            case ROOK -> chessPiece.color() == PlayerColor.WHITE ? '♖' : '♜';
+            case KNIGHT -> chessPiece.color() == PlayerColor.WHITE ? '♘' : '♞';
+            case BISHOP -> chessPiece.color() == PlayerColor.WHITE ? '♗' : '♝';
+            case QUEEN -> chessPiece.color() == PlayerColor.WHITE ? '♕' : '♛';
+            case KING -> chessPiece.color() == PlayerColor.WHITE ? '♔' : '♚';
+        };
     }
 }
--- Java/src/test/java/org/chess/presentation/rendering/console/ConsoleBoardRendererTest.java
+++ Java/src/test/java/org/chess/presentation/rendering/console/ConsoleBoardRendererTest.java
@@ -3,6 +3,8 @@ package org.chess.presentation.rendering.console;
 import org.chess.domain.GameState;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ConsoleBoardRendererTest {
@@ -10,7 +12,7 @@ class ConsoleBoardRendererTest {
     @Test
     void shouldRenderEmptyBoard() {
         final var renderer = new ConsoleBoardRenderer();
-        final var gameState = GameState.initialState();
+        final var gameState = new GameState(Map.of());
 
         var actual = renderer.render(gameState);
         var expected = """
@@ -31,6 +33,37 @@ class ConsoleBoardRendererTest {
                   2  ║     ║     ║     ║     ║     ║     ║     ║     ║
                      ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
                   1  ║     ║     ║     ║     ║     ║     ║     ║     ║
+                     ╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝
+                        a     b     c     d     e     f     g     h
+                """;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldRenderInitialPositions() {
+        final var renderer = new ConsoleBoardRenderer();
+        final var gameState = GameState.initialState();
+
+        var actual = renderer.render(gameState);
+        var expected = """
+                                   
+                     ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗
+                  8  ║  ♜  ║  ♞  ║  ♝  ║  ♛  ║  ♚  ║  ♝  ║  ♞  ║  ♜  ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  7  ║  ♟  ║  ♟  ║  ♟  ║  ♟  ║  ♟  ║  ♟  ║  ♟  ║  ♟  ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  6  ║     ║     ║     ║     ║     ║     ║     ║     ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  5  ║     ║     ║     ║     ║     ║     ║     ║     ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  4  ║     ║     ║     ║     ║     ║     ║     ║     ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  3  ║     ║     ║     ║     ║     ║     ║     ║     ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  2  ║  ♙  ║  ♙  ║  ♙  ║  ♙  ║  ♙  ║  ♙  ║  ♙  ║  ♙  ║
+                     ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣
+                  1  ║  ♖  ║  ♘  ║  ♗  ║  ♕  ║  ♔  ║  ♗  ║  ♘  ║  ♖  ║
                      ╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝
                         a     b     c     d     e     f     g     h
                 """;
