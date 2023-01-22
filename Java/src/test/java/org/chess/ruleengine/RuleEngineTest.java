245809f3a1fe8d21fcb8982f8b1d2ca7c9cedc34
Raimund Krämer <kraemer_raimund@web.de>
2023-01-22 12:57:39 +0100

    Make test package-private

--- Java/src/test/java/org/chess/ruleengine/RuleEngineTest.java
+++ Java/src/test/java/org/chess/ruleengine/RuleEngineTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RuleEngineTest {
+class RuleEngineTest {
 
     @Test
     void shouldRejectMoveIfAnyRuleIsViolated() {
