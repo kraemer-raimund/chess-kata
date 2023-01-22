789e5b844cf19ac7611c2b66c97c5b8d2eb1941f
Raimund Krämer <kraemer_raimund@web.de>
2023-01-22 13:04:01 +0100

    Test that move with non-empty source position is not rejected

--- Java/src/test/java/org/chess/domain/rules/SourcePositionMustNotBeEmptyTest.java
+++ Java/src/test/java/org/chess/domain/rules/SourcePositionMustNotBeEmptyTest.java
@@ -1,8 +1,6 @@
 package org.chess.domain.rules;
 
-import org.chess.domain.GameState;
-import org.chess.domain.Move;
-import org.chess.domain.Position;
+import org.chess.domain.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -21,5 +19,18 @@ class SourcePositionMustNotBeEmptyTest {
         var ruleViolation = rule.execute(move, gameState);
 
         assertThat(ruleViolation).isPresent();
+        assertThat(ruleViolation.get()).isEqualTo(RuleViolation.NO_PIECE_AT_SOURCE_POSITION);
+    }
+
+    @Test
+    void shouldNotRejectMoveWithChessPieceAtSourcePosition() {
+        var gameState = new GameState(Map.of(
+                new Position(0, 1), new ChessPiece(ChessPieceName.PAWN, PlayerColor.WHITE)));
+        var rule = new SourcePositionMustNotBeEmpty();
+
+        var move = new Move(new Position(0, 1), new Position(0, 5));
+        var ruleViolation = rule.execute(move, gameState);
+
+        assertThat(ruleViolation).isNotPresent();
     }
 }
