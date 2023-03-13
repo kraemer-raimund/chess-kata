eb93f419a4d4ce6e76249246b04eb3e4929d6fd7
Raimund Krämer <kraemer_raimund@web.de>
2023-03-13 18:38:30 +0100

    Port RuleViolation enum from Java to Kotlin

--- Kotlin/src/main/kotlin/org/chess/domain/RuleViolation.java
+++
@@ -1,6 +0,0 @@
-package org.chess.domain;
-
-public enum RuleViolation {
-    NO_PIECE_AT_SOURCE_POSITION,
-    TARGET_POSITION_OCCUPIED_BY_ALLY
-}
---
+++ Kotlin/src/main/kotlin/org/chess/domain/RuleViolation.kt
@@ -0,0 +1,5 @@
+package org.chess.domain
+
+enum class RuleViolation {
+    NO_PIECE_AT_SOURCE_POSITION, TARGET_POSITION_OCCUPIED_BY_ALLY
+}
