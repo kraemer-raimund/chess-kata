1d095f8ca6d4403154b38401a4c8407d367d703f
Raimund Krämer <kraemer_raimund@web.de>
2023-03-13 18:58:37 +0100

    Port TargetPositionMustNotBeOccupiedByAlly to Kotlin

---
+++ Kotlin/src/main/kotlin/org/chess/domain/rules/TargetPositionMustNotBeOccupiedByAlly.kt
@@ -0,0 +1,21 @@
+package org.chess.domain.rules
+
+import org.chess.domain.GameState
+import org.chess.domain.Move
+import org.chess.domain.RuleViolation
+import java.util.*
+
+class TargetPositionMustNotBeOccupiedByAlly : Rule {
+
+    override fun execute(move: Move, gameState: GameState): Optional<RuleViolation> {
+        val chessPieceAtTargetPosition = gameState
+            .chessPiecePositions
+            .getOrDefault(move.to(), null)
+
+        if (chessPieceAtTargetPosition != null && chessPieceAtTargetPosition.color() === gameState.currentPlayer) {
+            return Optional.of(RuleViolation.TARGET_POSITION_OCCUPIED_BY_ALLY)
+        }
+
+        return Optional.empty()
+    }
+}
