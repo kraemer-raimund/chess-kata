9c2a67238acd363b18ea6c1c074d68c5d599132c
Raimund Krämer <kraemer_raimund@web.de>
2023-03-13 19:01:22 +0100

    Port MoveResult Java record to Kotlin data class

--- Kotlin/src/main/kotlin/org/chess/ruleengine/MoveResult.java
+++
@@ -1,9 +0,0 @@
-package org.chess.ruleengine;
-
-import org.chess.domain.GameState;
-import org.chess.domain.RuleViolation;
-
-import java.util.Collection;
-
-public record MoveResult(GameState gameState, Collection<RuleViolation> violations) {
-}
