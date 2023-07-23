ee91221e704e6c4fba439d8c36234960173f3294
Raimund Kraemer <kraemer_raimund@web.de>
2023-07-23 12:53:39 +0200

    Add CSharp project with NUnit

---
+++ CSharp_Console/ChessTests/ExampleTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace ChessTests
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ShouldFail()
+        {
+            Assert.Fail();
+        }
+    }
+}
