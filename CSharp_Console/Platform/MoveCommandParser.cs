77813479991dd7fffd3d51cfc2736582ba41b1c6
Raimund Kraemer <kraemer_raimund@web.de>
2023-08-01 17:53:59 +0200

    Add MoveCommandParser that rejects all inputs

---
+++ CSharp_Console/Platform/MoveCommandParser.cs
@@ -0,0 +1,13 @@
+﻿namespace Chess.Platform
+{
+    public class MoveCommandParser
+    {
+        public void Parse(string userInput)
+        {
+            throw new ArgumentException(
+                $"Invalid move command `{userInput}`",
+                nameof(userInput)
+            );
+        }
+    }
+}
---
+++ CSharp_Console/PlatformTests/MoveCommandParserTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Chess.Platform.Tests
+{
+    internal class MoveCommandParserTestFixture
+    {
+        internal class TheParser
+        {
+            [TestCase("asdf")]
+            [TestCase("e0 -> e8")]
+            [TestCase("e1 -> e9)")]
+            [TestCase("c1 -> i5")]
+            [TestCase("c1 <- i5")]
+            public void RejectsInvalidInput(string userInput)
+            {
+                var parser = new MoveCommandParser();
+
+                Assert.Throws<ArgumentException>(() => parser.Parse(userInput));
+            }
+        }
+    }
+}
