6c1f2243b12c978a1bcd6012ba88faa204ae0fae
Raimund Kraemer <kraemer_raimund@web.de>
2023-08-01 17:27:24 +0200

    Implement Engine as top-level scaffolding
    There are no tests for this (especially no "test first" tests),
    since it is close to the infrastructure and platform and only
    holds everything together relatively loosely.
    
    Some architectural decisions, like not abstracting the renderer
    and not injecting dependencies, are intentional in order to be able
    to create additional kata exercises, like discussing possible
    architectural changes or problems.

---
+++ CSharp_Console/Platform/Engine.cs
@@ -0,0 +1,73 @@
+﻿using Chess.Domain;
+using Chess.Rendering;
+
+namespace Chess.Platform
+{
+    internal class Engine
+    {
+        private readonly ConsoleBoardRenderer _renderer = new ConsoleBoardRenderer();
+
+        private bool _running = false;
+
+        public void StartGame()
+        {
+            _running = true;
+            var initialState = GameState.InitialState();
+            GameLoop(initialState);
+        }
+
+        private void GameLoop(GameState initialState)
+        {
+            GameState gameState = initialState;
+            while (_running)
+            {
+                ClearScreen();
+                RenderToScreen(gameState);
+                PromptPlayerInput();
+                string playerInput = ReadPlayerInput();
+                gameState = HandlePlayerInput(playerInput, gameState);
+            }
+        }
+
+        private void ClearScreen()
+        {
+            Console.WriteLine(
+                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                "\n\n\n\n\n\n\n\n\n\n"
+            );
+        }
+
+        private void RenderToScreen(GameState gameState)
+        {
+            Console.WriteLine(_renderer.Render(gameState));
+        }
+
+        private void PromptPlayerInput()
+        {
+            Console.WriteLine("Type your move in the format \"from -> to\", e.g., \"e2 -> e8\".");
+        }
+
+        private string ReadPlayerInput()
+        {
+            try
+            {
+                return Console.ReadLine();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Failed to read player input. Exiting game.");
+                _running = false;
+                return "";
+            }
+        }
+
+        private GameState HandlePlayerInput(String playerInput, GameState gameState)
+        {
+            // For now, we do not transform the game state. The game state will
+            // remain the same for any player input. Later we will parse the
+            // player input and transform the game state according to the rules
+            // of chess.
+            return gameState;
+        }
+    }
+}
---
+++ CSharp_Console/Platform/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Chess.Platform
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(
+                "The game is about to begin. Please make sure your " +
+                "console is set to a font which can display unicode " +
+                "special characters. For example DejaVu Sans Mono " +
+                "on Windows."
+            );
+
+            var chessEngine = new Engine();
+            chessEngine.StartGame();
+        }
+    }
+}
