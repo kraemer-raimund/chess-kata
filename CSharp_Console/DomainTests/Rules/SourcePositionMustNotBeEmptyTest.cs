3531257959bd552868bd1aba3a01a2b862b15a1e
Raimund Kraemer <kraemer_raimund@web.de>
2023-08-02 17:20:23 +0200

    Test that rule accepts the positive case
    The SourcePositionMustNotBeEmpty rule does not reject the move if there
    is a chess piece at the source position.

--- CSharp_Console/DomainTests/Rules/SourcePositionMustNotBeEmptyTest.cs
+++ CSharp_Console/DomainTests/Rules/SourcePositionMustNotBeEmptyTest.cs
@@ -15,5 +15,17 @@ namespace Chess.Domain.Rules.Tests
 
             Assert.IsNotNull(ruleViolation);
         }
+
+        [Test]
+        public void AcceptsMoveWithChessPieceAtSourcePosition()
+        {
+            GameState gameState = GameState.InitialState();
+            SourcePositionMustNotBeEmpty rule = new();
+
+            var move = new Move(new(5, 1), new(5, 2));
+            RuleViolation? ruleViolation = rule.Execute(move, gameState);
+
+            Assert.IsNull(ruleViolation);
+        }
     }
 }
